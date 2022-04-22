4d99eff80a5eaae11ccdc78d9675800db9603bdc
Samantha Wette <samantha.wette@gmail.com>
2022-04-22 08:26:02 -0700

    forecast

--- static/ajax-review.js
+++ static/ajax-review.js
@@ -1,7 +1,6 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune() {
-  // TODO: get the fortune and show it in the fortune-text div
   fetch('/fortune')
   .then(response => response.text())
   .then(responseData => {
@@ -18,7 +17,12 @@ function showWeather(evt) {
   evt.preventDefault();
   const zipcode = document.querySelector('#zipcode-field').value;
   const url = `/weather?zipcode=${zipcode}`;
-  // TODO: request weather with that URL and show the forecast in #weather-info
-}
+  fetch(url)
+    .then(response=>response.json())
+    .then(responseJson => {
+      document.querySelector('#weather-info').innerHTML = responseJson.forecast
+    });
+    }
+
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
